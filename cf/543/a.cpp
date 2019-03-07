a9da996bcfd8317ac168c5cd96bcef1c63515302
CheaSim <852975133@qq.com>
2019-03-07 22:25:54 +0800

    home

--- cf/543/a.cpp
+++ cf/543/a.cpp
@@ -11,14 +11,26 @@ const int INF = 0x3f3f3f3f;
 
 int n,m,k;
 const int maxn = 110;
-
+int p[maxn];
+int s[maxn];
+int c[maxn];
+int sts[maxn];
 int main(){
 #ifdef LOCAL
     freopen("1.in","r",stdin);
 #endif
+    scanf("%d%d%d",&n,&m,&k);
+    rep(i,1,n+1) scanf("%d",p+i);
+    rep(i,1,n+1) scanf("%d",s+i);
+    rep(i,1,k+1) scanf("%d",c+i);
 
-
-
-
+    rep(i,1,n+1) {
+        sts[s[i]] = max(sts[s[i]],p[i]);
+    }
+    int ans = 0;
+    rep(i,1,k+1){
+        if(p[c[i]] < sts[s[c[i]]]) ans ++;
+    }
+    printf("%d\n",ans);
     return 0;
 }
---
+++ cf/543/c.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for(int i=(a);i<(n);i++)
+#define per(i,a,n) for(int i=(n-1);i>=(a);i--)
+#define fi first
+#define se second
+typedef pair <int,int> pII;
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+//head
+
+int main(){
+#ifdef LOCAL
+    freopen("3.in","r",stdin);
+#endif
+    
+    return 0;
+}  
