07e91ad3f473f548269244fd901abd5842838e4f
CheaSim <852975133@qq.com>
2019-03-07 19:04:41 +0800

    nothing

---
+++ cf/543/a.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for(int i=(a);i<(n);i++)
+#define per(i,a,n) for(int i=(n-1);i>=(a);i--)
+#define fi first
+#define se second
+typedef pair <int,int> pII;
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+//head
+
+int n,m,k;
+const int maxn = 110;
+
+int main(){
+#ifdef LOCAL
+    freopen("1.in","r",stdin);
+#endif
+
+
+
+
+    return 0;
+}
